f239237db5e158168fe04143dae39de787bc47d3
harshithacharya632-source <harshithacharya632@gmail.com>
2025-09-16 08:27:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -8,7 +8,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '25962591'))
 API_HASH = environ.get('API_HASH', '5d1dc00807bc62b346488a483bd3075d')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7011228023:AAG-cnuOaMMANjuV_hbDv3Ebfexd2RmJEFc")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7011228023:AAEkyjKftHr0XieSZbVSKF9qrsVQ4yi99Qg")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
@@ -184,6 +184,7 @@ else:
     OTHER_DB_URI = O_DB_URI       # This Db Is For Other Data Store
     FILE_DB_URI = F_DB_URI        # This Db Is For File Data Store
     SEC_FILE_DB_URI = S_DB_URI    # This Db is for File Data Store When First Db Is Going To Be Full.
+
 
 
 
