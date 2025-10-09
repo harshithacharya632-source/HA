08bb2921fba803f642654c8cf12c20ab62ecc987
harshithacharya632-source <harshithacharya632@gmail.com>
2025-10-09 20:49:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -58,12 +58,12 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://harshithacharya632:j2U
 DATABASE_NAME = environ.get('DATABASE_NAME', "Goflix")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
-MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
+MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
-O_DB_URI = environ.get('O_DB_URI', "")   # This Db Is For Other Data Store
-F_DB_URI = environ.get('F_DB_URI', "")   # This Db Is For File Data Store
-S_DB_URI = environ.get('S_DB_URI', "")   # This Db is for File Data Store When First Db Is Going To Be Full.
+O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://harshithacharya633_db_user:kQzjXvzRWOO8LumH@goflixbot2.44y9hzo.mongodb.net/?retryWrites=true&w=majority&appName=Goflixbot2")   # This Db Is For Other Data Store
+F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://harshithacharya633_db_user:kQzjXvzRWOO8LumH@goflixbot2.44y9hzo.mongodb.net/?retryWrites=true&w=majority&appName=Goflixbot2")   # This Db Is For File Data Store
+S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://harshithacharya633_db_user:kQzjXvzRWOO8LumH@goflixbot2.44y9hzo.mongodb.net/?retryWrites=true&w=majority&appName=Goflixbot2")   # This Db is for File Data Store When First Db Is Going To Be Full.
 
 
 # Premium And Referal Settings
@@ -184,6 +184,7 @@ else:
     OTHER_DB_URI = O_DB_URI       # This Db Is For Other Data Store
     FILE_DB_URI = F_DB_URI        # This Db Is For File Data Store
     SEC_FILE_DB_URI = S_DB_URI    # This Db is for File Data Store When First Db Is Going To Be Full.
+
 
 
 
