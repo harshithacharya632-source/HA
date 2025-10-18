5c0d7c81dae1e98066b1af12feaf9570ee90c3d7
harshithacharya632-source <harshithacharya632@gmail.com>
2025-10-18 10:15:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -149,7 +149,7 @@ YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998",
 
 
 # Online Stream and Download
-STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
+STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
@@ -159,7 +159,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://goflix-link.onrender.com")
+URL = environ.get("URL", "https://playerjet.com/thirdparty/?id=yourId&live=downloadLink/streaming")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
@@ -184,6 +184,7 @@ else:
     OTHER_DB_URI = O_DB_URI       # This Db Is For Other Data Store
     FILE_DB_URI = F_DB_URI        # This Db Is For File Data Store
     SEC_FILE_DB_URI = S_DB_URI    # This Db is for File Data Store When First Db Is Going To Be Full.
+
 
 
 
