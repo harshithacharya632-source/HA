ffc90b8eec966499a624f2869d83d3559de4308a
harshithacharya632-source <harshithacharya632@gmail.com>
2025-10-21 15:27:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,20 +107,20 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'yourdestinationlink.com&alias=CustomAlias')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '175a9da92b79af75bf3120f9feee208af8905620')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'https://shrinkme.io/api?api=175a9da92b79af75bf3120f9feee208af8905620')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/Goflix_AdminBot')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
-VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', True))
+VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 # if verify second shortner is True then fill below url and api
 VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'yourdestinationlink.com&alias=CustomAlias')
 VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '175a9da92b79af75bf3120f9feee208af8905620')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'yourdestinationlink.com&alias=CustomAlias')
 SHORTLINK_API = environ.get('SHORTLINK_API', '175a9da92b79af75bf3120f9feee208af8905620')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Goflix_AdminBot') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
@@ -184,6 +184,7 @@ else:
     OTHER_DB_URI = O_DB_URI       # This Db Is For Other Data Store
     FILE_DB_URI = F_DB_URI        # This Db Is For File Data Store
     SEC_FILE_DB_URI = S_DB_URI    # This Db is for File Data Store When First Db Is Going To Be Full.
+
 
 
 
