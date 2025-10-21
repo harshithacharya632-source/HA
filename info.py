b159e9b2914da65738cf7a8fd610923943694b3a
harshithacharya632-source <harshithacharya632@gmail.com>
2025-10-21 15:36:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -108,8 +108,8 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', True))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'yourdestinationlink.com&alias=CustomAlias')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'https://shrinkme.io/api?api=175a9da92b79af75bf3120f9feee208af8905620')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'SHORTLINK_URL = https://shrinkme.io/api?api=175a9da92b79af75bf3120f9feee208af8905620&url=')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'https://shrinkme.io/api?api=175a9da92b79af75bf3120f9feee208af8905620&url=')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/Goflix_AdminBot')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
@@ -184,6 +184,7 @@ else:
     OTHER_DB_URI = O_DB_URI       # This Db Is For Other Data Store
     FILE_DB_URI = F_DB_URI        # This Db Is For File Data Store
     SEC_FILE_DB_URI = S_DB_URI    # This Db is for File Data Store When First Db Is Going To Be Full.
+
 
 
 
