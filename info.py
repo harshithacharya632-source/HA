94d68b00e9500db0c38cd273b1b18b71ff3d1f56
harshithacharya632-source <harshithacharya632@gmail.com>
2026-01-02 16:01:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -159,7 +159,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://thin-celestina-goflix-1e523ecf.koyeb.app/")
+URL = environ.get("URL", "https://thin-celestina-goflix-1e523ecf.koyeb.app")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
@@ -184,6 +184,7 @@ else:
     OTHER_DB_URI = O_DB_URI       # This Db Is For Other Data Store
     FILE_DB_URI = F_DB_URI        # This Db Is For File Data Store
     SEC_FILE_DB_URI = S_DB_URI    # This Db is for File Data Store When First Db Is Going To Be Full.
+
 
 
 
