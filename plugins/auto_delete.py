4ab4bf51e70fae9b9966e200a7f60b8d08c35d69
harshithacharya632-source <harshithacharya632@gmail.com>
2026-02-02 18:27:39 +0530

    Update auto_delete.py

--- plugins/auto_delete.py
+++ plugins/auto_delete.py
@@ -1,13 +1,17 @@
-# Auto delete user messages after 3 minutes
+# Auto delete user messages after 3 minutes (NON-BLOCKING)
+
 import asyncio
 from pyrogram import Client, filters
 
-@Client.on_message(
-    filters.private & ~filters.bot
-)
+@Client.on_message(filters.private & filters.incoming & ~filters.bot)
 async def auto_delete_user_messages(client, message):
-    await asyncio.sleep(180)  # 3 minutes
-    try:
-        await message.delete()
-    except:
-        pass
+
+    async def delete_later(msg):
+        await asyncio.sleep(180)  # 3 minutes
+        try:
+            await msg.delete()
+        except:
+            pass
+
+    # run in background so other handlers still work
+    asyncio.create_task(delete_later(message))
