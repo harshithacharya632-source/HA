27cde02addc0b3bf8d49e442be7301017c1cfecb
harshithacharya632-source <harshithacharya632@gmail.com>
2026-02-02 18:42:18 +0530

    Update auto_delete.py

--- plugins/auto_delete.py
+++ plugins/auto_delete.py
@@ -1,13 +1,29 @@
-# Auto delete user messages after 3 minutes (SAFE VERSION)
+# Auto delete user messages after 3 minutes (PRIVATE + GROUP)
 
 import asyncio
 from pyrogram import Client, filters
+from pyrogram.enums import ChatMemberStatus
 
 @Client.on_message(
-    filters.private & filters.incoming & ~filters.bot,
-    group=100   # VERY IMPORTANT
+    filters.incoming & ~filters.bot,
+    group=100
 )
 async def auto_delete_user_messages(client, message):
+
+    # ❌ Skip admins in groups (Telegram rule)
+    if message.chat.type in ["group", "supergroup"]:
+        try:
+            member = await client.get_chat_member(
+                message.chat.id,
+                message.from_user.id
+            )
+            if member.status in (
+                ChatMemberStatus.ADMINISTRATOR,
+                ChatMemberStatus.OWNER
+            ):
+                return
+        except:
+            return
 
     async def delete_later(msg):
         await asyncio.sleep(180)  # 3 minutes
