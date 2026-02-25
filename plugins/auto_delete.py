8bf21c3358305c3cde95c13870dcb0b0d14a7fd6
harshithacharya632-source <harshithacharya632@gmail.com>
2026-02-25 12:52:49 +0530

    Update auto_delete.py

--- plugins/auto_delete.py
+++ plugins/auto_delete.py
@@ -1,35 +1,35 @@
-# Auto delete user messages after 3 minutes (PRIVATE + GROUP)
+# # Auto delete user messages after 3 minutes (PRIVATE + GROUP)
 
-import asyncio
-from pyrogram import Client, filters
-from pyrogram.enums import ChatMemberStatus
+# import asyncio
+# from pyrogram import Client, filters
+# from pyrogram.enums import ChatMemberStatus
 
-@Client.on_message(
-    filters.incoming & ~filters.bot,
-    group=100
-)
-async def auto_delete_user_messages(client, message):
+# @Client.on_message(
+#     filters.incoming & ~filters.bot,
+#     group=100
+# )
+# async def auto_delete_user_messages(client, message):
 
-    # ❌ Skip admins in groups (Telegram rule)
-    if message.chat.type in ["group", "supergroup"]:
-        try:
-            member = await client.get_chat_member(
-                message.chat.id,
-                message.from_user.id
-            )
-            if member.status in (
-                ChatMemberStatus.ADMINISTRATOR,
-                ChatMemberStatus.OWNER
-            ):
-                return
-        except:
-            return
+#     # ❌ Skip admins in groups (Telegram rule)
+#     if message.chat.type in ["group", "supergroup"]:
+#         try:
+#             member = await client.get_chat_member(
+#                 message.chat.id,
+#                 message.from_user.id
+#             )
+#             if member.status in (
+#                 ChatMemberStatus.ADMINISTRATOR,
+#                 ChatMemberStatus.OWNER
+#             ):
+#                 return
+#         except:
+#             return
 
-    async def delete_later(msg):
-        await asyncio.sleep(200)  # 3 minutes
-        try:
-            await msg.delete()
-        except:
-            pass
+#     async def delete_later(msg):
+#         await asyncio.sleep(200)  # 3 minutes
+#         try:
+#             await msg.delete()
+#         except:
+#             pass
 
-    asyncio.create_task(delete_later(message))
+#     asyncio.create_task(delete_later(message))
