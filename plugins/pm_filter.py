06321ace8552317a4a606b63ae26a051cf002d76
harshithacharya632-source <harshithacharya632@gmail.com>
2026-05-25 09:37:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1198,7 +1198,7 @@ async def filter_qualities_cb_handler(client: Client, query: CallbackQuery):
         except MessageNotModified:
             pass
                 
-@Client.on_callback_query()
+@Client.on_callback_query(group=1)
 async def cb_handler(client: Client, query: CallbackQuery):
     if query.data == "close_data":
         await query.message.delete()
