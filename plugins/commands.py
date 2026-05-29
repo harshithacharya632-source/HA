8b9352e5e434588eff035b4cb8c5134b7eda12a5
harshithacharya632-source <harshithacharya632@gmail.com>
2026-05-29 10:50:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -521,7 +521,7 @@ async def start(client, message):
     user = message.from_user.id
     files_ = await get_file_details(file_id)           
     if not files_:
-        pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
+        pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("utf-8")).split("_", 1)
         try:
             if not await db.has_premium_access(message.from_user.id):
                 if not await check_verification(client, message.from_user.id) and VERIFY == True:
