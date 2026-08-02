ad8061c5f92a588fc57ab4786dec89edeef0871c
harshithacharya632-source <harshithacharya632@gmail.com>
2026-08-02 11:09:39 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -72,35 +72,17 @@ async def pub_is_subscribed(bot, query, channel):
 
 
 async def is_subscribed(bot, query):
-    if REQUEST_TO_JOIN_MODE == True and join_db().isActive():
-        try:
-            user = await join_db().get_user(query.from_user.id)
-            if user and user["user_id"] == query.from_user.id:
-                return True
-            else:
-                try:
-                    user_data = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-                except UserNotParticipant:
-                    pass
-                except Exception as e:
-                    logger.exception(e)
-                else:
-                    if user_data.status != enums.ChatMemberStatus.BANNED:
-                        return True
-        except Exception as e:
-            logger.exception(e)
-            return False
-    else:
-        try:
-            user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-        except UserNotParticipant:
-            pass
-        except Exception as e:
-            logger.exception(e)
-        else:
-            if user.status != enums.ChatMemberStatus.BANNED:
-                return True
+    try:
+        user_data = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+    except UserNotParticipant:
         return False
+    except Exception as e:
+        logger.exception(e)
+        return False
+    else:
+        if user_data.status != enums.ChatMemberStatus.BANNED:
+            return True
+    return False
 
 
 async def get_poster(query, bulk=False, id=False, file=None):
